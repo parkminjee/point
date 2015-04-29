09e17dd333cb0a0a5a52ea663fa61fd0ce44cdc7
박민지 <minjee9292@naver.com>
2015-04-29 16:57:28 +0900

    first commit

---
+++ src/point/ColorPoint.java
@@ -0,0 +1,30 @@
+package point;
+
+public class ColorPoint extends Point{
+	private String color;
+	
+	public ColorPoint(int x, int y, String color){
+		setX(x);
+		setY(y);
+		
+//		super(x,y);
+		this.color=color;
+	}
+	
+	public ColorPoint(){
+		System.out.println("call ClolrPoint()");
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	public void show(){
+		System.out.println("좌표 [x = "+super.getX()+", y= "+super.getY()+"]에 "+color+"점을 그렸습니다.");
+	}
+
+}
